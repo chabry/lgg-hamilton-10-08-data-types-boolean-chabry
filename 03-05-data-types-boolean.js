f2e65a74f16d861b5984e3d8b8ec8fa685de9333
Chabry <chabry01@gmail.com>
2024-11-22 14:44:40 +0100

    Exercice résolu

--- 03-05-data-types-boolean.js
+++ 03-05-data-types-boolean.js
@@ -3,21 +3,29 @@
 // Function 1
 function returnTrue() {
   // Your code here
+  return true
 }
 
 // Function 2
 function returnFalse() {
   // Your code here
+  return false
 }
 
 // Function 3
 function negateBoolean(value) {
   // Your code here
+    return !value
 }
 
 // Function 4
 function compareBooleans(bool1, bool2) {
   // Your code here
+  if (bool1 == bool2){
+    return true
+  }else{
+    return false
+  }
 }
 
 module.exports = {
